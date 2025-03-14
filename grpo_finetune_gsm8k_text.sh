e4ea3a02d5b71a78b27e1bca40f3dea890777b79
Noah682 <noahwei682@gmail.com>
2025-03-14 15:05:59 +0800

    Create grpo_finetune_gsm8k_text.sh

---
+++ grpo_finetune_gsm8k_text.sh
@@ -0,0 +1,35 @@
+conda create -n llava-next python=3.10 -y
+source activate llava-next
+pip install --upgrade pip  # Enable PEP 660 support.
+
+git clone https://github.com/EvolvingLMMs-Lab/lmms-eval
+cd lmms-eval
+pip install -e .
+
+git clone https://github.com/noahwei682/llava-ov-ewc-grpo.git
+cd llava-ov-ewc-grpo
+
+pip install -e ".[train]"
+pip install flash-attn==2.5.2 --no-build-isolation
+
+conda install -c conda-forge wandb --yes
+wandb login a0686d210ceba8f713f6cd85c5dcf3621b7f15e7
+
+export HF_TOKEN=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
+export HF_HOME=/data/harold/mhj/huggingface
+huggingface-cli login --token $HF_TOKEN
+
+export OMP_NUM_THREADS=8
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_SOCKET_IFNAME=br-intranet
+export NCCL_DEBUG=INFO
+
+export ACCELERATE_CPU_AFFINITY=1 
+export NPROC_PER_NODE=8 
+export NODES=1 
+export NODE_RANK=0 
+export MASTER_ADDR=172.17.100.112 
+export MASTER_PORT=23456 
+
+bash ./scripts/train/grpo_finetune_gsm8k.sh
