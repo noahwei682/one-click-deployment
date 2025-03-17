155fba4ea25693ac935b1b5f0a6692b825bfd1d1
Noah682 <noahwei682@gmail.com>
2025-03-18 01:06:59 +0800

    Update run_eval_ewc.sh

--- run_eval_ewc.sh
+++ run_eval_ewc.sh
@@ -1,5 +1,4 @@
 conda create -n llava-next python=3.10 -y
-source activate llava-next
 pip install --upgrade pip  # Enable PEP 660 support.
 
 git clone https://github.com/EvolvingLMMs-Lab/lmms-eval
@@ -12,7 +11,6 @@ cd llava-ov-ewc/
 pip install -e ".[train]"
 pip install flash-attn==2.5.2 --no-build-isolation
 pip install ipdb
-
 
 git clone https://github.com/noahwei682/download_data.git
 cd download_data
@@ -40,24 +38,36 @@ cd ..
 mkdir huggingface
 
 conda install -c conda-forge wandb --yes
+pip install --upgrade wandb
 wandb login a0686d210ceba8f713f6cd85c5dcf3621b7f15e7
 
 export HF_TOKEN=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
-export HF_HOME=/data/harold/mhj/huggingface
+# export HF_HOME=/data/harold/mhj/huggingface
+# export HF_HOME=/scratch
+pip install huggingface_hub
 huggingface-cli login --token $HF_TOKEN
 
 export OMP_NUM_THREADS=8
-export NCCL_IB_DISABLE=0
+export NCCL_IB_DISABLE=1
 export NCCL_IB_GID_INDEX=3
-export NCCL_SOCKET_IFNAME=br-intranet
+export NCCL_SOCKET_IFNAME="eth0"
 export NCCL_DEBUG=INFO
 
 export ACCELERATE_CPU_AFFINITY=1 
-export NPROC_PER_NODE=8 
+export NPROC_PER_NODE=4 
 export NODES=1 
 export NODE_RANK=0 
-export MASTER_ADDR=172.17.100.112 
-export MASTER_PORT=23456 
+export MASTER_ADDR="10.36.38.89"
+export MASTER_PORT="23456"  
+
+# export SERVER_IP="10.36.38.89"
+# export SERVER_PORT="23456"
+# export NETWORK_INTERFACE="eth0"
+
+# export SERVER_IP="127.0.0.1/8"
+# export SERVER_PORT="8080"
+# export NETWORK_INTERFACE="lo"
+
 
 # bash ./scripts/train/finetune_grpo_gsm8k.sh
 bash ./scripts/train/finetune_ov_FigureQA_MathV360K.sh
