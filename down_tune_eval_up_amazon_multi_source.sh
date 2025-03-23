3faab4da0e642e887ea92d13e6987525ae2ea0f5
Noah682 <noahwei682@gmail.com>
2025-03-23 11:36:01 +0800

    Update down_tune_eval_up_amazon_multi_source.sh

--- down_tune_eval_up_amazon_multi_source.sh
+++ down_tune_eval_up_amazon_multi_source.sh
@@ -5,17 +5,12 @@ git clone https://github.com/EvolvingLMMs-Lab/lmms-eval
 cd lmms-eval
 pip install -e .
 
-git clone https://github.com/noahwei682/llava-ov-ewc.git
-cd llava-ov-ewc/
+git clone https://github.com/noahwei682/llava-ov-ewc-ms.git
+cd llava-ov-ewc-ms/
 
 pip install -e ".[train]"
 pip install flash-attn==2.5.2 --no-build-isolation
 pip install ipdb
-
-
-
-
-
 
 
 pip install huggingface_hub
@@ -23,24 +18,26 @@ export HF_TOKEN=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
 huggingface-cli login --token $HF_TOKEN
 # local-dir=/home/aiscuser
 
+mkdir msdata
+huggingface-cli download wei682/amazon images_20250321.tar.gz --repo-type dataset  --local-dir ./msdata
+cd msdata
+mkdir images
+tar -xvzf images_20250321.tar.gz -C ./images
 
-huggingface-cli download wei682/amazon images_20250321.tar.gz --repo-type dataset  --local-dir [your data_path]
-tar -xvzf images_20250321.tar.gz -C ./image/meta_Gift_Cards_pictures
-
-huggingface-cli download wei682/amazon meta_Electronics_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Arts_Crafts_and_Sewing_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Industrial_and_Scientific_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Health_and_Household_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Movies_and_TV_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Beauty_and_Personal_Care_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Electronics_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Appliances_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Cell_Phones_and_Accessories_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Grocery_and_Gourmet_Food_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Sports_and_Outdoors_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Magazine_Subscriptions_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Subscription_Boxes_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Toys_and_Games_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_All_Beauty_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Patio_Lawn_and_Garden_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
-huggingface-cli download wei682/amazon meta_Gift_Cards_exist_gene_ITdata.json --repo-type dataset  --local-dir [your data_path]
+huggingface-cli download wei682/amazon meta_Electronics_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Arts_Crafts_and_Sewing_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Industrial_and_Scientific_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Health_and_Household_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Movies_and_TV_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Beauty_and_Personal_Care_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Electronics_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Appliances_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Cell_Phones_and_Accessories_exist_gene_ITdata.json --repo-type dataset .
+huggingface-cli download wei682/amazon meta_Grocery_and_Gourmet_Food_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Sports_and_Outdoors_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Magazine_Subscriptions_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Subscription_Boxes_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Toys_and_Games_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_All_Beauty_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Patio_Lawn_and_Garden_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+huggingface-cli download wei682/amazon meta_Gift_Cards_exist_gene_ITdata.json --repo-type dataset  --local-dir .
