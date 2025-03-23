f1028a3aa964a73ace868aaa4cf04d395f55939c
Noah682 <noahwei682@gmail.com>
2025-03-23 11:52:01 +0800

    Update down_tune_eval_up_amazon_multi_source.sh

--- down_tune_eval_up_amazon_multi_source.sh
+++ down_tune_eval_up_amazon_multi_source.sh
@@ -1,4 +1,6 @@
 conda create -n llava-next python=3.10 -y
+source activate llava-next
+conda activate llava-next
 pip install --upgrade pip  # Enable PEP 660 support.
 
 git clone https://github.com/EvolvingLMMs-Lab/lmms-eval
@@ -41,3 +43,34 @@ huggingface-cli download wei682/amazon meta_Toys_and_Games_exist_gene_ITdata.jso
 huggingface-cli download wei682/amazon meta_All_Beauty_exist_gene_ITdata.json --repo-type dataset  --local-dir .
 huggingface-cli download wei682/amazon meta_Patio_Lawn_and_Garden_exist_gene_ITdata.json --repo-type dataset  --local-dir .
 huggingface-cli download wei682/amazon meta_Gift_Cards_exist_gene_ITdata.json --repo-type dataset  --local-dir .
+
+export NCCL_IB_DISABLE=1
+export NCCL_DEBUG=INFO
+export ACCELERATE_CPU_AFFINITY=1 
+export NPROC_PER_NODE=4
+export NODES=1 
+export NODE_RANK=0 
+
+export RUN_NAME="llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all"
+export OUTPUT_DIR="/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all"
+export PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-7b-si"
+
+bash ./scripts/train/finetune_ov_multisource_amazon.sh
+
+huggingface-cli upload LLaVA_checkpoint /blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all ./qwen2-7b-si-ewc-lambda01-amazon-multisource-all --repo-type dataset
+sudo apt-get install jq
+>
+  jq '. + {"vocab_size": 152064}' \
+  /blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all/config.json \
+  > /blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all/tmp.json && \
+  mv /blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all/tmp.json \
+  /blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all/config.json
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks scienceqa_img --batch_size 1 --log_samples --log_samples_suffix scienceqa_img_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks gqa --batch_size 1 --log_samples --log_samples_suffix gqa_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks mmbench_en --batch_size 1 --log_samples --log_samples_suffix mmbench_en_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks mmbench_cn --batch_size 1 --log_samples --log_samples_suffix mmbench_cn_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks pope --batch_size 1 --log_samples --log_samples_suffix pope_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks mme --batch_size 1 --log_samples --log_samples_suffix mme_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks seedbench --batch_size 1 --log_samples --log_samples_suffix seedbench_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+python3 -m accelerate.commands.launch --num_processes 8 -m lmms_eval --model llava --model_args pretrained=/blob/weiwei/llava_checkpoint/llava-onevision-google-siglip-so400m-patch14-384-lmms-lab-llava-onevision-qwen2-7b-si-ewc-lambda01-amazon-multisource-all --tasks vizwiz_vqa --batch_size 1 --log_samples --log_samples_suffix vizwiz_vqa_si-ewc-lambda01-amazon-multisource-all --output_path ./logs/ --verbosity=DEBUG --hf_hub_log_args hub_repo_name=lm-eval-results,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False 
+
